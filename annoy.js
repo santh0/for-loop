26572a9f6338d263e6411647cbb06ae921c6ba32
santh0 <santhoshvj37@gmail.comgit config --global user.email santhoshvj37@gmail.comgit config --global user.email santhoshvj37@gmail.com>
2019-10-18 18:55:56 +0530

    updated

---
+++ annoy.js
@@ -0,0 +1,13 @@
+var answer =prompt ("are we there yet?");
+while(answer !=="yes"){
+	var answer =prompt ("are we there yet?");
+}
+
+alert ("YaY, you made it"); 
+//if(answer === "yes")
+//{
+//	alert("YaY, we made it")
+//}
+//else{
+//	alert{"no!!!"}
+//}
