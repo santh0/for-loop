9182cddca68c9b82d8284f78d14406a934830535
santh0 <santhoshvj37@gmail.comgit config --global user.email santhoshvj37@gmail.comgit config --global user.email santhoshvj37@gmail.com>
2019-10-19 18:31:27 +0530

    first commit

---
+++ loop.js
@@ -0,0 +1,26 @@
+/*for (var i =-10 ; i <10; i++) {
+	console.log(i)
+}
+alert("connect")*/
+
+// for(var i= 10; i <40; i+=2)
+// {
+// 	console.log(i)
+// }
+
+
+// for(i=300;i<333;i+=1)
+// 	if (i%2!==0) 
+// {
+// 	console.log(i)
+
+// }
+
+
+
+
+for(var i= 5; i<=50; i++)
+{
+	if(i%5===0&&i%3===0)
+		console.log(i)
+}
